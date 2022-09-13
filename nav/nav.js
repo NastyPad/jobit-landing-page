4cc4be51bf2b647648efbe935e6551003a5f0bda
Leonardo Manuel Grau Vargas <leonardo.grau@outlook.com.pe>
2022-09-13 15:09:11 -0500

    feat: dunno

--- nav/nav.js
+++ nav/nav.js
@@ -1,6 +1,9 @@
 const sidenav = document.querySelector("#sidenav");
-var insideNav = document.querySelectorAll("#sidenav > *");
+
 const nav = document.querySelector("#navbar > .list-container");
+sidenav.innerHTML += nav.innerHTML + `<div></div>`;
+
+var insideNav = document.querySelectorAll("#sidenav > *");
 const buttonCloseSideNavbar = document.getElementById("button-close");
 const buttonOpenSideNavBar = document.getElementById("button-list");
 
@@ -9,8 +12,8 @@ function openSideNav() {
   //console.log(sidenav.clientWidth);
   if(sidenav.clientWidth == 0) {
     buttonCloseSideNavbar.style.display = "block";
-    sidenav.style.width = `${window.innerWidth * 0.3}px`;
-    for(var element of document.querySelectorAll("#sidenav > *")) {
+    sidenav.style.width = `${document.documentElement.clientWidth * 0.3}px`;
+    for(var element of insideNav) {
       element.style.visibility = "visible";
     }
   }
@@ -20,14 +23,14 @@ function closeSideNav() {
   if(sidenav.clientWidth != 0) {
     buttonCloseSideNavbar.style.display = "none";
     sidenav.style.width = `0px`;
-    for(var element of document.querySelectorAll("#sidenav > *")) {
+    for(var element of insideNav) {
       element.style.visibility = "hidden";
     }
   }
 }
 
 function main() {
-  sidenav.innerHTML += nav.innerHTML + `<div></div>`;
+  
 }
 
 main();
